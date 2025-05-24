6e4a9b8118db1853ca64c975a9dff31fe8065ba1
kikulabo <kikuchi@x-tech5.co.jp>
2025-05-24 17:06:03 +0900

    feat: ec2 を追加

--- learn-cdk-steps/workshop/lib/workshop-stack.ts
+++ learn-cdk-steps/workshop/lib/workshop-stack.ts
@@ -9,5 +9,14 @@ export class WorkshopStack extends Stack {
     const vpc = new ec2.Vpc(this, 'VPC', {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
     });
+
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+    });
+
   }
 }
