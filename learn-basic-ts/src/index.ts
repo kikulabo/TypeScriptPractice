4f11389415e5c695992b4e7e65409d9a800f6d41
kikulabo <kikuchi@x-tech5.co.jp>
2025-06-15 21:06:30 +0900

    feat: スプレッド構文

--- learn-basic-ts/src/index.ts
+++ learn-basic-ts/src/index.ts
@@ -1,17 +1,11 @@
-import { createInterface } from 'readline';
+const obj1 = {
+    bar: 456,
+    baz: 789
+};
 
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const obj2 = {
+    foo: 123,
+    ...obj1
+};
 
-const messages = {
-    good: "0以上の数値が入力されました！",
-    bad: "負の数値を入力しないでください！"
-}
-
-rl.question('数値を入力してください:', (line) => {
-    const num = Number(line);
-    console.log(messages[num >= 0 ? "good" : "bad"]);
-    rl.close();
-});
+console.log(obj2);
