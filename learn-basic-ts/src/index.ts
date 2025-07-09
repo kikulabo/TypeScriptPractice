3ab942b9a5cad0d9eadd26de1c0a51afb9ef98fb
kikulabo <kikuchi@x-tech5.co.jp>
2025-07-09 23:46:41 +0900

    feat: User1クラスを追加し、SuperUser1クラスのエラーを修正

--- learn-basic-ts/src/index.ts
+++ learn-basic-ts/src/index.ts
@@ -1,15 +1,17 @@
-type HasName = {
-    name: string;
+class User1 {
+    private age = 0;
 }
 
-class User implements HasName {
-    name: string;
-    #age: number;
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.#age = age;
-    }
-    public isAdult(): boolean {
-        return this.#age >= 20;
-    }
+// これはエラー
+class SuperUser1 extends User1 {
+    private age = 1;
+}
+
+class User2 {
+    #age = 0;
+}
+
+// これはOK
+class SuperUser2 extends User2 {
+    #age = 1;
 }
