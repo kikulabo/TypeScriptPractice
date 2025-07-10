a5cfeaf70874955e27c2395897515c604663f703
kikulabo <kikuchi@x-tech5.co.jp>
2025-07-10 22:22:05 +0900

    feat: Userクラスを修正し、SuperUser1クラスを削除

--- learn-basic-ts/src/index.ts
+++ learn-basic-ts/src/index.ts
@@ -1,17 +1,15 @@
-class User1 {
-    private age = 0;
+class User {
+    name: string;
+    #age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.#age = age;
+    }
+    public isAdult(): boolean {
+        return this.#age >= 20;
+    }
 }
 
-// これはエラー
-class SuperUser1 extends User1 {
-    private age = 1;
-}
-
-class User2 {
-    #age = 0;
-}
-
-// これはOK
-class SuperUser2 extends User2 {
-    #age = 1;
-}
+const uhyo = new User("uhyo", 26);
+const isAdult = uhyo.isAdult;
+console.log(isAdult());
