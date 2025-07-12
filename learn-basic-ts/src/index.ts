cdf8780542853c1e0a47a1077e04f6fe49fc0bab
kikulabo <kikuchi@x-tech5.co.jp>
2025-07-12 23:10:23 +0900

    feat: Userクラスを定義し、ユーザー作成とメッセージ取得のメソッドを実装

--- learn-basic-ts/src/index.ts
+++ learn-basic-ts/src/index.ts
@@ -1,21 +1,15 @@
-type User = {
-  name: string;
-  age: number;
-}
+class User {
+    readonly name: string;
+    readonly age: number;
+    constructor(name: string, age: number) {
+        if (name === "") {
+            throw new Error("名前は空にできません！");
+        }
+        this.name = name;
+        this.age = age;
+    }
 
-function createUser(name: string, age: number): User {
-  if (name === "") {
-    throw new Error("名前は空にできません！");
-  }
-  return {
-    name,
-    age
-  };
+    getMessage(message: string): string {
+        return `${this.name} (${this.age}) 「${message}」`;
+    }
 }
-
-function getMessage(user: User, message: string): string {
-  return `${user.name} (${user.age}) 「${message}」`;
-}
-
-const uhyo = createUser("uhyo", 26);
-console.log(getMessage(uhyo, "こんにちは"));
