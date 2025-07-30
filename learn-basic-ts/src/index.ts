66ba56d28c426a5d554807df0fb602611b0c2440
kikulabo <kikuchi@x-tech5.co.jp>
2025-07-30 23:54:35 +0900

    feat: Human型に年齢プロパティを追加し、uhyoの初期化を修正

--- learn-basic-ts/src/index.ts
+++ learn-basic-ts/src/index.ts
@@ -1,9 +1,11 @@
-type Human = { name: string };
-type Animal = { species: string };
-function getName(human: Human) {
-    return human.name;
-}
-function getSpecies(animal: Animal) {
-    return animal.species;
-}
-const mysteryFunc = Math.random() < 0.5 ? getName : getSpecies;
+type Human = {
+    name: string;
+    age?: number;
+};
+const uhyo: Human = {
+    name: "uhyo",
+    age: 25
+};
+const john: Human = {
+    name: "John Smith"
+};
