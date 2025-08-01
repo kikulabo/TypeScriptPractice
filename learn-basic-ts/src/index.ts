d49308d7fbe978c0b2294f79df14bcc2a9227639
kikulabo <kikuchi@x-tech5.co.jp>
2025-08-01 23:39:21 +0900

    feat: Human型の年齢プロパティをオプショナルから必須に変更し、初期化を修正

--- learn-basic-ts/src/index.ts
+++ learn-basic-ts/src/index.ts
@@ -1,11 +1,16 @@
 type Human = {
     name: string;
-    age?: number;
+    age: number ¦ undefined;
 };
 const uhyo: Human = {
     name: "uhyo",
     age: 25
 };
 const john: Human = {
-    name: "John Smith"
+    name: "John Smith",
+    age: undefined
+};
+const taro: Human = {
+    name: "Taro Yamada",
+    age: undefined
 };
