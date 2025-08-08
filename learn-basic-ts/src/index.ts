584b8aeec62853a589c3bb9db9dfac4bf1071836
kikulabo <kikuchi@x-tech5.co.jp>
2025-08-08 23:57:03 +0900

    feat: getHelloStr関数を削除し、makeKey関数を追加

--- learn-basic-ts/src/index.ts
+++ learn-basic-ts/src/index.ts
@@ -1,12 +1,4 @@
-function getHelloStr(): `Hello, ${string}!` {
-    const rand = Math.random();
-    if (rand < 0.3) {
-        return "久しぶりの登壇!";
-    } else if (rand < 0.6) {
-        return "お疲れ様でした!";
-    } else if (rand < 0.9) {
-        return "Hello, world!";
-    } else {
-        return "Hell, world!";
-    }
+function makeKey<T extends string>(userName: T) {
+    return `user:${userName}` as const;
 }
+const uhyoKey: "user:uhyo" = makeKey("uhyo");
